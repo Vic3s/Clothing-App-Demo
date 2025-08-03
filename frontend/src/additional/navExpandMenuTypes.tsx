09f15070102327244c641ffab9f9b547e230e0e9
VaskoDaGama <vasko4da4gama@gmail.com>
2025-08-03 19:42:01 +0300

    Add an expnading menu for navbar clothing options on hover almost completely functional, still needs some work tough.

---
+++ frontend/src/additional/navExpandMenuTypes.tsx
@@ -0,0 +1,94 @@
+import { Link } from "react-router-dom"
+
+
+export const ManClothingTypes = `
+    <li><Link to={"/"} className="link-underline link-underline-opacity-0 link-secondary">See All</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">New</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">T-Shirts</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Hoodies</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Jeans</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Trousers</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Shorts</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Swimwear</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Sweaters</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Shirts</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Outerwear</Link></li>
+`
+
+export const WomanClothingTypes = `
+    <li><Link to={"/"} className="link-underline link-underline-opacity-0 link-secondary">See All</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">New</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">T-Shirts</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Tops</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Hoodies</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Sweaters</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Shirts</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Dresses</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Skirts</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Jeans</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Trousers</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Shorts</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Swimwear</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Outerwear</Link></li>
+`
+
+export const ManShoeTypes = `
+    <li><Link to={"/"} className="link-underline link-underline-opacity-0 link-secondary">See All</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">New</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Sneakers</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Sports Shoes</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Formal Shoes</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Slippers</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Flip Flops</Link></li>
+`
+
+
+export const WomanShoeTypes = `
+    <li><Link to={"/"} className="link-underline link-underline-opacity-0 link-secondary">See All</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">New</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Sneakers</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Sports Shoes</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Heels</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Slippers</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Flip Flops</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Boots</Link></li>
+`
+
+export const ManAccessoriesTypes = `
+    <li><Link to={"/"} className="link-underline link-underline-opacity-0 link-secondary">See All</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">New</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Jewelry</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Pijamas</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Backpacks</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Underwear</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Socks</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Hats</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Belts</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Wallets</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Glasses</Link></li>
+`
+
+export const WomanAccessoriesTypes = `
+    <li><Link to={"/"} className="link-underline link-underline-opacity-0 link-secondary">See All</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">New</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Jewelry</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Pijamas</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Bags, Backpacks</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Underwear</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Bathrobes</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Socks, Pantyhose</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Hats</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Belts</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Wallets</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Glasses</Link></li>
+`
+
+export const Brands = `
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Brand 1</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Brand 2</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Brand 3</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Brand 4</Link></li>
+    <li><Link to={"/"} className=" link-underline link-underline-opacity-0 link-secondary">Brand 5</Link></li>
+`
+
+// REMEMBER TO ADD THE SMAE LIST OF HOT ITEMS FOR HOT OPTION IN NAV BAR
--- frontend/src/partials/NavBar.tsx
+++ frontend/src/partials/NavBar.tsx
@@ -1,14 +1,81 @@
 import "./partials-styles/additional-styling.css"
 import derssIcom from "../assets/dress-svg.svg"
 import { Link } from "react-router"
+import { useRef, useState, useEffect, type JSX } from "react"
+import ReactDOMServer from "react-dom/server";
+
+//images imports 
+import male_model_nav_menu from "../assets/male_model_nav_menu.jpg"
+import female_model_nav_menu from "../assets/female_model_nav_menu.jpg"
+
+
+import {ManClothingTypes, ManShoeTypes, ManAccessoriesTypes, WomanClothingTypes, WomanShoeTypes, WomanAccessoriesTypes, Brands} from "../additional/navExpandMenuTypes"
 
 export const NavBar = () => {
+
+    const navExpandMenu = useRef<HTMLDivElement>(null);
+
+    const[clothesList, setClothesList] = useState<string>("");
+    const[shoesList, setShoesList] = useState<string>("");
+    const[accessoriesList, setAccessoriesList] = useState<string>("");
+    const[brandsList, setBrandsList] = useState<string>("");
+
+
+    const ExpandMenuActive = () => {
+        if(navExpandMenu.current){
+            navExpandMenu.current.classList.remove("nav-expand-menu-inactive")
+            navExpandMenu.current.classList.add("nav-expand-menu-active")
+        }
+    }
+    const ExpandMenuInactive = () => {
+        if(navExpandMenu.current){
+
+            navExpandMenu.current.innerHTML = `
+            <ul class="clothes-list list-unstyled">
+                <li><h4 class="fw-bold">Clothes</h4></li>
+                ${clothesList}
+            </ul>
+            <ul class="shoes-list list-unstyled">
+                <li><h4 class="fw-bold">Shoes</h4></li>
+                ${shoesList}
+            </ul>
+            <ul class="accessories-list list-unstyled">
+                <li><h4 class="fw-bold">Accessories</h4></li>
+                ${accessoriesList}
+            </ul>
+            <ul class="brands-list list-unstyled">
+                <li><h4 class="fw-bold">Brands</h4></li>
+                ${brandsList}
+            </ul>
+            `
+            navExpandMenu.current.classList.remove("nav-expand-menu-active")
+            navExpandMenu.current.classList.add("nav-expand-menu-inactive")
+        }
+    }
+    const ExpandMenuActiveHot = () => {
+        if(navExpandMenu.current){
+            navExpandMenu.current.innerHTML = `
+            <div class="card border-0 nav-card-item d-flex align-items-center" >
+                <img class="card-img-top" src=${male_model_nav_menu} alt="Male Model Image Navbar">
+                <h3 class="card-body fw-bold">For Him</h3>
+            </div>
+            <div class="card border-0 nav-card-item d-flex align-items-center">
+                <img class="card-img-top" src=${female_model_nav_menu} alt="Female Model Image Navbar">
+                <h3 class="card-body fw-bold">For Her</h3>
+            </div>
+        `
+        }
+    }
+
+    useEffect(() => {
+        setBrandsList(Brands)
+    }, [])
 
     return (
     <>
     <nav className="navbar navbar-expand-lg navbar-dark gradient-custom" style={{"height": "110px"}}>
     <div className="container">
-        <Link className="navbar-brand text-dark" to="/">Home</Link>
+        <Link className="navbar-brand text-dark d-flex flex-column justify-content-center" to="/">Home</Link>
 
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
         data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
@@ -18,24 +85,35 @@ export const NavBar = () => {
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto d-flex flex-row mt-3 mt-lg-0">
-            <li className="nav-item text-center mx-2 mx-lg-1">
-            <a className="nav-link active text-dark" aria-current="page" href="#!">
+            <li className="nav-item text-center mx-2 mx-lg-1" onMouseEnter={() => {
+                
+                setClothesList(ManClothingTypes);
+                setShoesList(ManShoeTypes);
+                setAccessoriesList(ManAccessoriesTypes);
+                ExpandMenuActive();
+            }} onMouseLeave={ExpandMenuInactive}>
+            <a className="nav-link active text-dark d-flex flex-column justify-content-center" aria-current="page" href="#!">
                 <div>
                     <i className="fa-solid fa-shirt fa-lg" style={{"color": "#280028"}}></i>
                 </div>
                 Him
             </a>
             </li>
-            <li className="nav-item text-center mx-2 mx-lg-1">
-            <a className="nav-link active text-dark" href="#!">
+            <li className="nav-item text-center mx-2 mx-lg-1" onMouseEnter={() => {
+                setClothesList(WomanClothingTypes);
+                setShoesList(WomanShoeTypes);
+                setAccessoriesList(WomanAccessoriesTypes);
+                ExpandMenuActive();
+            }} onMouseLeave={ExpandMenuInactive}>
+            <a className="nav-link active text-dark d-flex flex-column justify-content-center" href="#!">
                 <div>
                     <img src={derssIcom} alt="dress-icon" className="dress-icon" />
                 </div>
                 Her
             </a>
             </li>
-            <li className="nav-item text-center mx-2 mx-lg-1">
-            <a className="nav-link active text-dark" href="#!">
+            <li className="nav-item text-center mx-2 mx-lg-1" onMouseEnter={() => {ExpandMenuActiveHot(); ExpandMenuActive()}} onMouseLeave={ExpandMenuInactive}>
+            <a className="nav-link active text-dark d-flex flex-column justify-content-center" href="#!">
                 <div>
                     <i className="fa-solid fa-fire fa-2xl" style={{"color": "#280028"}}></i>
                 </div>
@@ -43,7 +121,7 @@ export const NavBar = () => {
             </a>
             </li>
             <li className="nav-item text-center mx-2 mx-lg-1">
-            <Link to="/customize" className="nav-link active text-dark">
+            <Link to="/customize" className="nav-link active text-dark d-flex flex-column justify-content-center">
                 <div>
                     <i className="fa-solid fa-person fa-2xl" style={{"color": "#280028"}}></i>
                 </div>
@@ -60,7 +138,7 @@ export const NavBar = () => {
 
         <ul className="navbar-nav ms-auto d-flex flex-row mt-3 mt-lg-0">
             <li className="nav-item text-center mx-2 mx-lg-1">
-            <a className="nav-link text-dark" href="#!">
+            <a className="nav-link text-dark d-flex flex-column justify-content-center" href="#!">
                 <div>
                 <i className="fa-solid fa-circle-user fa-2xl" style={{"color": "#280028"}}></i>
                 </div>
@@ -76,11 +154,27 @@ export const NavBar = () => {
         </form>
         </div>
     </div>
-
-    
     </nav>
-        
+    <div ref={navExpandMenu} onMouseEnter={ExpandMenuActive} onMouseLeave={ExpandMenuInactive} className="nav-expand-menu-inactive d-flex 
+    flex-row position-absolute justify-content-around" style={{"zIndex": "1"}}>
+        <ul className="clothes-list list-unstyled">
+            <li><h4 className="fw-bold">Clothes</h4></li>
+            {/* {clothesList} */}
+        </ul>
+        <ul className="shoes-list list-unstyled">
+            <li><h4 className="fw-bold">Shoes</h4></li>
+            {shoesList}
+        </ul>
+        <ul className="accessories-list list-unstyled">
+            <li><h4 className="fw-bold">Accessories</h4></li>
+            {accessoriesList}
+        </ul>
+        <ul className="brands-list list-unstyled">
+            <li><h4 className="fw-bold">Brands</h4></li>
+            {brandsList}
+        </ul>
+    </div>
+
     </>
     )
-
 }
